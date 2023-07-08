70847a041931c71c0d22c162e2d1cc1846ec20d4
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-08 11:36:16 +0200

    Ustawianie wartości zmiennych, Boolean

---
+++ main.py
@@ -0,0 +1,30 @@
+# Day 17 Boolean
+
+a, b, c = "Orange", "Banana", "Cherry"   # ustawianie wartości kilku zmiennych
+print(a)
+print(b)
+print(c)
+
+d = e = f = "Orange"
+print(d)
+print(e)
+print(f)
+
+# Boolean
+
+print(11 > 1)  # czy 11 jest większe niż jeden
+print(11 == 1)  # czy 11 jest równe jeden
+print(11 < 1)  # czy 11 jest mniejsze od 1
+print(11 != 1)  # czy 11 jest różne niż jeden
+
+a, b, c = "green", "blue", "black"
+print(a, b)
+print(c)
+print(c,b,a)
+
+print(5 > 6)
+print(2.3 < 1.3)
+print(2.5 == 6.6)
+print(3.4 !=0.2)
+
+
